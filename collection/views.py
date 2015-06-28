895fddb7380b59627f34af6cceb3b1788f614bcc
fifart <fifart@gmail.com>
2015-06-28 14:15:00 +0300

    Just Before the Forms

--- collection/views.py
+++ collection/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from collection.models import Thing
 
 # Create your views here.
 def index(request):
-    thing = "Thing Name"
-    number= 1
-    return render(request, 'index.html', { 'number': number, 'thing': thing, })
+    things = Thing.objects.all()
+    return render(request, 'index.html', {'things': things, })
+
+def thing_detail(request, slug):
+    thing = Thing.objects.get(slug=slug)
+    return render(request, 'things/thing_detail.html',
+    {
+        'thing':thing,
+        })
